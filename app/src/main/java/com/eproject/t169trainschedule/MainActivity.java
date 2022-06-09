1aa637319760e9b3a547bceb20a87869e6a010e5
Zeerex23 <tobilobadiekola@gmail.com>
2022-06-09 03:24:22 +0100

    Feat: Added Splash Screen Animation

--- app/src/main/java/com/eproject/t169trainschedule/MainActivity.java
+++ app/src/main/java/com/eproject/t169trainschedule/MainActivity.java
@@ -4,10 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Animation fadeIn;
+    private TextView splashText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +22,20 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
         setContentView(R.layout.activity_main);
 
+        // Initialize view
+        splashText = findViewById(R.id.splash_text);
+
+        // Initialize animation
+        fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+        splashText.setVisibility(TextView.INVISIBLE);
+        splashText.startAnimation(fadeIn);
+
+        // Delay for 2 seconds
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(MainActivity.this, HomeActivity.class);
             startActivity(intent);
             finish();
-        }, 2000);
+        }, 3000);
     }
 }
